686dd07d33e071ec6aad9df82a5c25bde5be2f7e
Christopher Amavisca and Ryan Spore <pair+camavisca+ryan@pivotallabs.com>
2014-10-30 15:09:27 -0700

    attempt to reproduce #1085

--- src/main/java/com/example/activity/MainActivity.java
+++ src/main/java/com/example/activity/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.activity;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.widget.DatePicker;
 import com.example.R;
 
 public class MainActivity extends Activity {
@@ -11,5 +16,17 @@ public class MainActivity extends Activity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
     setTitle("Deckard");
+    new DialogFragment() {
+      @Override
+      public Dialog onCreateDialog(Bundle savedInstanceState) {
+        new DatePickerDialog(MainActivity.this, android.R.style.Theme_Holo_Dialog, new DatePickerDialog.OnDateSetListener() {
+          @Override
+          public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+          }
+        }, 2014, 9, 26);
+        return super.onCreateDialog(savedInstanceState);
+      }
+    }.show(getFragmentManager(), "face");
   }
 }
